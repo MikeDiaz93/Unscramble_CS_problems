1bd0798502b15981ceaba6b1d276c53327a13dbc
MikelDietz <mikeoikos121@comunidad.unam.mx>
2021-02-21 21:05:26 -0600

    Update task_2.py
    Add the real task_2 code.

--- task_2.py
+++ task_2.py
@@ -1,33 +1,42 @@
 """
 Read file into texts and calls.
-It's ok if you don't understand how to read files.
+It's ok if you don't understand how to read files
 """
 import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
+
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-
 """
-TASK 1:
-How many different telephone numbers are there in the records? 
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
 Print a message:
-"There are <count> different telephone numbers in the records."
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
 """
 
+totals = dict()
+for call in calls:
+    phone_caller = call[0]
+    phone_reciever = call[1]
+    duration_call = int(call[3])
 
-list_numb = []
-for i in range(0, len(texts)):
-    list_numb.append(texts[i][0])
-    list_numb.append(texts[i][1])
+    if phone_caller in totals:
+        totals[phone_caller] += int(duration_call)
+    else:
+        totals[phone_caller] = int(duration_call)
 
-for i in range(0, len(calls)):
-    list_numb.append(calls[i][0])
-    list_numb.append(calls[i][1])
+    if phone_reciever in totals:
+        totals[phone_reciever] += duration_call
+    else:
+        totals[phone_reciever] = duration_call
 
-count = len(set(list_numb))
+lngst_time_phone = max(totals.items(), key=lambda x: int(x[1]))
 
-print(f"There are {count} different telephone numbers in the records.")
+print(f"{lngst_time_phone [0]} spent the longest time, {lngst_time_phone [1]} \
+seconds, on the phone during September 2016.")
